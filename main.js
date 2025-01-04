79b946a2af5c979324015f9c83c6a809e908c552
Steve S. Lee <sjlee12@me.com>
2025-01-04 14:23:21 -0500

    working app initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// console.log('Hello from Electron 👋')
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadURL('http://localhost:3000')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
